0ccec991529f7113e5137555abd533e454a4880c
Elijah Luckey <luckeyelijah112@gmail.com>
2023-07-09 16:53:44 -0400

    staging

--- lib/ynab_api.dart
+++
@@ -1,5 +0,0 @@
-/// A Very Good Project created by Very Good CLI.
-library ynab_api;
-
-export 'generated/spec_v1_swagger.enums.swagger.dart';
-export 'generated/spec_v1_swagger.models.swagger.dart';
